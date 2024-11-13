34438fcfa64f004ccf3f00da58d5b4f21ffe27bf
4b3g0134@office.stust.edu.tw <4b3g0134@office.stust.edu.tw>
2024-11-13 21:05:01 +0800

    輸入一個正整數n，請以三種重複結構(for, while, do-while)來計算小於等於n的所有偶數和。

--- ex.1/ex.1.cpp
+++ ex.1/ex.1.cpp
@@ -2,25 +2,35 @@
 using namespace std;
 
 int main() {
-    int month;
-    cout << "請輸入月份 (1-12): ";
-    cin >> month;
+    int n;
+    cout << "請輸入一個正整數 n: ";
+    cin >> n;
 
-    if (month >= 3 && month <= 5) {
-        cout << "這是春季。" << endl;
+    // 使用 for 迴圈
+    int sumFor = 0;
+    for (int i = 2; i <= n; i += 2) {
+        sumFor += i;
     }
-    else if (month >= 6 && month <= 8) {
-        cout << "這是夏季。" << endl;
+    cout << "使用 for 迴圈的偶數和: " << sumFor << endl;
+
+    // 使用 while 迴圈
+    int sumWhile = 0;
+    int i = 2;
+    while (i <= n) {
+        sumWhile += i;
+        i += 2;
     }
-    else if (month >= 9 && month <= 11) {
-        cout << "這是秋季。" << endl;
-    }
-    else if (month == 12 || month == 1 || month == 2) {
-        cout << "這是冬季。" << endl;
-    }
-    else {
-        cout << "輸入無效，請輸入 1 到 12 之間的數字。" << endl;
-    }
+    cout << "使用 while 迴圈的偶數和: " << sumWhile << endl;
+
+    // 使用 do-while 迴圈
+    int sumDoWhile = 0;
+    i = 2;
+    do {
+        if (i > n) break;  // 檢查避免超過 n
+        sumDoWhile += i;
+        i += 2;
+    } while (i <= n);
+    cout << "使用 do-while 迴圈的偶數和: " << sumDoWhile << endl;
 
     return 0;
 }
